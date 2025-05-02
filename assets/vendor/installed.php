a1a2e05c999f0c9157fe5c177c1ad69334dc6b05
ghassensaddem <ghassen.saddem@esprit.tn>
2025-05-02 01:24:17 +0100

    croud + api + métier avancé

---
+++ assets/vendor/installed.php
@@ -0,0 +1,101 @@
+<?php return array (
+  '@hotwired/stimulus' => 
+  array (
+    'version' => '3.2.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@hotwired/turbo' => 
+  array (
+    'version' => '7.3.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'leaflet' => 
+  array (
+    'version' => '1.9.4',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'leaflet/dist/leaflet.min.css' => 
+  array (
+    'version' => '1.9.4',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+      0 => '/dist/images/layers.png',
+      1 => '/dist/images/layers-2x.png',
+      2 => '/dist/images/marker-icon.png',
+    ),
+  ),
+  'intl-messageformat' => 
+  array (
+    'version' => '10.7.16',
+    'dependencies' => 
+    array (
+      0 => 'tslib',
+      1 => '@formatjs/fast-memoize',
+      2 => '@formatjs/icu-messageformat-parser',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'tslib' => 
+  array (
+    'version' => '2.8.1',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@formatjs/fast-memoize' => 
+  array (
+    'version' => '2.2.7',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@formatjs/icu-messageformat-parser' => 
+  array (
+    'version' => '2.11.2',
+    'dependencies' => 
+    array (
+      0 => 'tslib',
+      1 => '@formatjs/icu-skeleton-parser',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@formatjs/icu-skeleton-parser' => 
+  array (
+    'version' => '1.8.14',
+    'dependencies' => 
+    array (
+      0 => 'tslib',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+);
